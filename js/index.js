3e975781fdb5ef6acf943a94556c2509caade5ce
Jordan Hickling <jordan.hickling@netmatters-scs.com>
2022-10-04 12:17:36 +0100

    Updated 04/10/2022

--- js/index.js
+++ js/index.js
@@ -59,9 +59,11 @@ form.addEventListener('submit', (e) => {
         e.preventDefault();
     if (fname.value === "" || fname.value == null) {
         document.getElementById("fname").style.borderColor = "red";
-        alert("First name is required");
+        document.getElementById("warning-form-fname").style.display="inline"
+        // alert("First name is required");
     } else {
         document.getElementById("fname").style.borderColor = "green";
+        document.getElementById("warning-form-fname").style.display="none"
     }
   }) 
 
@@ -69,9 +71,11 @@ form.addEventListener('submit', (e) => {
         e.preventDefault();
     if (sname.value === "" || sname.value == null) {
         document.getElementById("sname").style.borderColor = "red";
-        alert("Last name is required")
+        document.getElementById("warning-form-lname").style.display="inline"
+        // alert("Last name is required")
     } else {
         document.getElementById("sname").style.borderColor = "green";
+        document.getElementById("warning-form-lname").style.display="none"
     }
 }) 
 
@@ -80,12 +84,17 @@ const regex = /(^(?!.*__.*)[a-z0-9]{4,253}(_?)[a-z0-9]+(?:\.[a-z0-9!#$%&*+\/=?^`
 form.addEventListener('submit', (e) =>{
 if (email.value === "" || email.value == null) { 
     document.getElementById("email").style.borderColor = "red";
-    alert("Email is required");
+    document.getElementById("warning-form-email").style.display="inline"
+    // alert("Email is required");
 } else if (email.value.match(regex)) {
     document.getElementById("email").style.borderColor = "green";
+    document.getElementById("warning-form-email").style.display="none"
+    document.getElementById("warning-form-regex").style.display="none"
 } else {
+    document.getElementById("warning-form-email").style.display="none"
     document.getElementById("email").style.borderColor = "red";
-    alert("This is an invalid Email Address! Please enter another or check it over.")
+    document.getElementById("warning-form-regex").style.display="inline"
+    // alert("This is an invalid Email Address! Please enter another or check it over.")
 }
 });
 
@@ -94,9 +103,11 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
     if (subject.value === "" || subject.value == null) {
         document.getElementById("subject").style.borderColor = "red";
-        alert("Subject is required"); 
+        document.getElementById("warning-form-subject").style.display="inline";
+        // alert("Subject is required"); 
     } else {
         document.getElementById("subject").style.borderColor = "green";
+        document.getElementById("warning-form-subject").style.display="none";
     }
 }) 
 
@@ -104,15 +115,17 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
     if (message.value === "" || message.value == null) {
         document.getElementById("message").style.borderColor = "red";
-        alert("A message is required");
+        document.getElementById("warning-form-message").style.display="inline";
+        // alert("A message is required");
     } else {
         document.getElementById("message").style.borderColor = "green";
+        document.getElementById("warning-form-message").style.display="none";
     }
 }) 
 
-
-
-
+// form.addEventListener('submit', (e) => {
+//     document.getElementById("success-popup").style.display="inl"
+// })
 
 // ================================================================== 
 //
